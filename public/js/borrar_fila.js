f1a5021f4935519e69fca44c722609fb90df9fac
and21094 <and21094@gmail.com>
2016-11-19 13:01:35 -0600

    Limpieza de repositorio

--- public/js/borrar_fila.js
+++
@@ -1,7 +0,0 @@
-/*$(document).ready(function(){
-    $("#prod").hover(function(){
-        $(this).css("background-color", "yellow");
-        }, function(){
-        $(this).css("background-color", "pink");
-    });
-});*/
--- public/js/cliente.js
+++
@@ -1,8 +0,0 @@
-function NuevoCliente(producto){
-
-	//llenar Modal nuevo cliente
-	$("#tituloModal").html(producto['descripcionProducto']);
-	$("#imagenModal").attr('src',producto['imagen']);
-	$("#codigo").text(producto['codigo']);
-
-}
